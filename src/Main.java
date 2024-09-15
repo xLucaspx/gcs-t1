ac8aef33352809b98484f05bab0630990835d925
Lucas da Paz <xlucaspx@gmail.com>
2024-09-15 13:20:35 -0300

    creating project structure
    Creating docs directory and src directory; updating readme.

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import app.App;
+
+/**
+ * Classe principal do sistema.
+ *
+ * @author Lucas da Paz
+ */
+public class Main {
+	/**
+	 * Instancia um objeto da classe de aplicação e chama seu
+	 * método <code>executar()</code>.
+	 *
+	 * @param args Argumentos recebidos do sistema operacional
+	 *             no início da execução do programa.
+	 */
+	public static void main(String... args) {
+		new App().executar();
+	}
+}
