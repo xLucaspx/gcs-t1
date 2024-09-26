4f116b2afe2dd7bd41e3ce582d25a0d69e833575
Gustavo de Freitas Fidelis <gustavo.fidelis@edu.pucrs.br>
2024-09-25 21:29:22 -0300

    item done

--- src/model/Item.java
+++ src/model/Item.java
@@ -1,7 +1,11 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Item {
 	private int id;
+	private static int gerador;
 	private String nome;
 	private String descricao;
 	private String categoria;
@@ -9,12 +13,21 @@ public class Item {
 
 	public Item(int id, String nome, String descricao, String categoria, float preco) {
 		this.id = id;
+		if(gerador < id) gerador = id;
 		this.nome = nome;
 		this.descricao = descricao;
 		this.categoria = categoria;
 		this.preco = preco;
 	}
 
+	public Item(String nome, String descricao, String categoria, float preco){
+		this.nome = nome;
+		this.descricao = descricao;
+		this.categoria = categoria;
+		this.preco = preco;
+		gerador++;
+		this.id = gerador;
+	}
 	/**
 	 * @return as informações do item em formato de String.
 	 */
