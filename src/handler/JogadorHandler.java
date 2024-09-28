c7bd42fef570a0a9895dc779670ee1ec3c8f3da3
Lucas da Paz <xlucaspx@gmail.com>
2024-09-28 16:50:21 -0300

    add modificadores na lista de jogadores em JogadorHandler

--- src/handler/JogadorHandler.java
+++ src/handler/JogadorHandler.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JogadorHandler {
-	List<Jogador> jogadores;
+	private final List<Jogador> jogadores;
 
 	public JogadorHandler() {
 		this.jogadores = new ArrayList<>();
