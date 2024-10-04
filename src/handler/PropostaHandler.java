ccabb85217987a77bb44ce7de111d1ed63f8775f
Luca W.B. <lucabohneberger@gmail.com>
2024-10-04 18:10:52 -0300

    add docs

--- src/handler/PropostaHandler.java
+++ src/handler/PropostaHandler.java
@@ -15,25 +15,38 @@ public class PropostaHandler {
 		this.propostas = new ArrayList<>();
 	}
 
-	public int getNumeroPropostasFinalizadas() {
-		int retorno = 0;
-		for (Proposta p : propostas) {
-			if (p.getStatus().equals("CONFIRMADA") || p.getStatus().equals("RECUSADA")) {
-				retorno++;
-			}
-		}
-		return retorno;
-	}
+	/**
+ * Retorna o número de propostas finalizadas pelo jogador.
+ * Propostas são consideradas finalizadas se o status for "CONFIRMADA" ou "RECUSADA".
+ *
+ * @return Número de propostas finalizadas
+ */
+public int getNumeroPropostasFinalizadas() {
+    int retorno = 0;
+    for (Proposta p : propostas) {
+        if (p.getStatus().equals("CONFIRMADA") || p.getStatus().equals("RECUSADA")) {
+            retorno++;
+        }
+    }
+    return retorno;
+}
 
-	public int getNumeroPropostasAbertas() {
-		int retorno = 0;
-		for (Proposta p : propostas) {
-			if (p.getStatus().equals("ABERTA")) {
-				retorno++;
-			}
-		}
-		return 0;
-	}
+/**
+ * Retorna o número de propostas abertas pelo jogador.
+ * Propostas são consideradas abertas se o status for "ABERTA".
+ *
+ * @return Número de propostas abertas
+ */
+public int getNumeroPropostasAbertas() {
+    int retorno = 0;
+    for (Proposta p : propostas) {
+        if (p.getStatus().equals("ABERTA")) {
+            retorno++;
+        }
+    }
+    return retorno;
+}
+
 
 	public boolean cadastra(Proposta p) {
 		if (p == null) {
